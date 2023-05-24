2bb2bd8b00dae9f6cbb99e4f15a21be528804940
Zeyad Essam <72177003+zezo-rgb@users.noreply.github.com>
2023-05-24 19:22:57 +0300

    Update signin.js

--- js/signin.js
+++ js/signin.js
@@ -32,7 +32,7 @@ document.getElementById("signupForm").addEventListener("submit", function(event)
       "Username":email,
       "Password":password,
     }
-    const url  = 'http://localhost:8000/user/'+user_Data.Username+'-'+user_Data.Password
+    const url  = 'http://localhost:80/user/'+user_Data.Username+'-'+user_Data.Password
   http.open("GET" , url , true)
   console.log(user_Data)
   http.send()
