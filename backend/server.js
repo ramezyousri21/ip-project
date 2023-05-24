082a4b20c23656114e693dfa94cfb28055669bd4
Ramez Yousri <ramez.yousri92@gmail.com>
2023-05-24 19:59:38 +0300

    'firstinit'

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bp = require('body-parser')
+const path = require('path');
+const app = express()
+const {Search , Update , AddNewData , Delete} = require('./backend')
+app.use(express.json());  
+app.use(bp.urlencoded({ extended: true }));
+
+
+
+
+app.post('/user/:username-:password-:fullname-:nationality' , (req , res)=>{
+    //console.log({"Name":req.params.username , "Password":req.params.password})
+    
+    AddNewData({"Name":req.params.username , "Password":req.params.password , "Nationality":req.params.nationality , "FullName":req.params.fullname})
+    res.send(Search(req.params.username))
+})
+app.get('/user/:username-:password' , (req , res)=>{
+    user = Search(req.params.username)
+    pass = user[0].Password
+    if(user){
+        if(req.params.password==pass){
+            console.log("Correct")
+        }
+        else{
+            console.log("Wrong Password")   
+        }
+    }else{
+        console.log("User Not Found")
+    }
+})
+app.delete('/user/:username' , (req , res)=>{
+    res.send(Delete(req.params.username))
+})
+
+app.put('/user/:username-:newdata' , (req , res)=>{
+    Update(req.params.username , req.params.newdata)
+    res.send(Delete(req.params.username))
+})
+
+app.listen(8000 , ()=>{
+    console.log("Listening....")
+})
---
+++ js/signin.js
@@ -0,0 +1,44 @@
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    var password = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+    var passwordError = document.getElementById("passwordError");
+    var emailError = document.getElementById("emailError");
+  
+    // Password length validation
+    if (password.length < 8) {
+      passwordError.textContent = "Password must be at least 8 characters long.";
+      return;
+    } else {
+      passwordError.textContent = "";
+    }
+  
+    // Email format validation
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+    if (!email.match(emailRegex)) {
+      emailError.textContent = "Invalid email address.";
+      return;
+    } else {
+      emailError.textContent = "";
+    }
+  
+    
+  
+  console.log("Sending data")
+    //Data Sending to API backend
+    const http = new XMLHttpRequest()
+    const user_Data = {
+      "Username":email,
+      "Password":password,
+    }
+    const url  = 'http://localhost:8000/user/'+user_Data.Username+'-'+user_Data.Password
+  http.open("GET" , url , true)
+  console.log(user_Data)
+  http.send()
+  
+  // Form submission
+  //this.submit();
+  
+  });
+  
